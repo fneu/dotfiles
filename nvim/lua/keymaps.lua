ea53232f0fca7a172ca61c84755d6e6f79ec446f
Fabian Neuschmidt <fabian@neuschmidt.de>
2022-05-26 21:12:27 +0200

    nvim: make terminal workable

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 
 vim.keymap.set('n', '<leader>ri', '<cmd>lua reload_config()<CR>', {desc = 'Reload init.lua'})
+
+
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {desc = 'Exit terminal-mode'})
---
+++ nvim/lua/options.lua
@@ -0,0 +1,3 @@
+if vim.fn.has('win32') == 1 then
+    vim.opt.shell='powershell.exe'
+end
