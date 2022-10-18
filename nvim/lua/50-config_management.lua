b6c5a6e02f58a80047ade5b22fd21b6e8726c8a6
Fabian Neuschmidt <fabian@neuschmidt.de>
2022-10-18 10:30:25 +0200

    refactor config management

--- nvim/lua/50-config_management.lua
+++ nvim/lua/50-config_management.lua
@@ -1,13 +1,16 @@
 function _G.reload_config()
-  dot_dir = find_dotfiles()
-  if dot_dir then
-    vim.notify(dot_dir.."/install.ps1")
-    if vim.fn.empty(vim.fn.glob(vim.fn.stdpath('config'))) < 1 then
-        vim.notify("nvim_dir exists, removing...")
-        os.execute('rd /s/q "'..vim.fn.stdpath('config')..'"')
+  old_config_dir = vim.fn.stdpath('config')
+  dotfiles = find_dotfiles()
+  if dotfiles then
+    new_config_dir = dotfiles..'/nvim'
+    if vim.fn.empty(vim.fn.glob(old_config_dir)) < 1 then
+        vim.notify(old_config_dir.." exists, removing...", vim.log.levels.INFO)
+        os.execute('rd /s/q "'..old_config_dir..'"')
     end
-    vim.notify("copying files")
-    os.execute('robocopy '..dot_dir..'/nvim '..vim.fn.stdpath('config')..' /s /xx')
+    vim.notify("copying files from "..new_config_dir, vim.log.levels.INFO)
+    os.execute('robocopy '..new_config_dir..' '..old_config_dir..' /s /xx')
+  else
+    vim.notify("no dotfiles found, loading current "..old_config_dir, vim.log.levels.INFO)
   end
   -- all packages starting with 00- to 99- will be reloaded, and init.lua will be reread.
   -- this might fail to update some plugin configs as their packages will not be reloaded
@@ -20,11 +23,14 @@ function _G.reload_config()
   vim.notify("init.lua reloaded!", vim.log.levels.INFO)
 end
 
--- reload config
 vim.keymap.set(
     'n',
-    '<leader>ri',
+    '<leader>ii',
     '<cmd>lua reload_config()<CR>', 
     {desc = 'Reload init.lua'})
     
- 
+vim.keymap.set(
+    'n',
+    '<leader>ie',
+    '<cmd>e '..find_dotfiles()..'/nvim/lua<CR>', 
+    {desc = 'Edit nvim config in dotfiles'})
