ae7240a576d79b2f209f4f4671479b5472949d09
Fabian Neuschmidt <fabian@neuschmidt.de>
2022-11-11 20:18:06 +0100

    nvim: Add telescope, fzf-native

---
+++ nvim/lua/10-plugins.lua
@@ -0,0 +1,77 @@
+local PKGS = {
+    -- the Paq package manager itself
+    'savq/paq-nvim';
+    -- :Gui... commands for nvim-qt
+    -- this should be included, but manually install seems more reliable
+    'equalsraf/neovim-gui-shim';
+    -- Colorscheme
+    'mcchrish/zenbones.nvim';
+    -- Zenbones customization
+    'rktjmp/lush.nvim';
+    -- Jump movements
+    -- s..
+    -- dx..
+    'ggandor/leap.nvim';
+    -- show hex and rgb colors in their respective color
+    'brenoprata10/nvim-highlight-colors';
+    -- color matching parentheses.
+    -- supported by zenbones
+    'p00f/nvim-ts-rainbow';
+    -- git
+    'tpope/vim-fugitive';
+    -- language understanding through syntax trees
+    -- integrates with multiple other plugins
+    {'nvim-treesitter/nvim-treesitter', run=TSUpdate};
+    -- File tree
+    -- TODO: decide between this, netrw, vinegar, and maybe telescope-something?
+    -- this copys and pastes easy, which netrw cannot do without customization on windows
+    -- git support is mediocre, cannot show new unstanged changes in a staged file
+    {'nvim-neo-tree/neo-tree.nvim', branch='v2.x'};
+    -- necessary for neotree
+    'MunifTanjim/nui.nvim';
+    -- necessary lua functions for neotree and telescope
+    'nvim-lua/plenary.nvim';
+    -- icons, need modified 'nerd font' 
+    'kyazdani42/nvim-web-devicons';
+    -- extensible fuzzy finder
+    {'nvim-telescope/telescope.nvim', branch='0.1.x'};
+    -- faster filtering for telescope
+    {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }; -- scoop install make mingw
+}
+
+-- bootstrap package manager if required
+local path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
+if vim.fn.empty(vim.fn.glob(path)) > 0 then
+    paq_bootstrap = vim.fn.system { 'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', path }
+    vim.cmd('packadd paq-nvim')
+end
+
+-- load and optionally install
+local paq = require('paq'):setup({verbose=true})
+paq(PKGS)
+if paq_bootstrap then
+    paq.install()
+end
+
+-- treesitter
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = {"c_sharp", "python", "lua", "json", "markdown"},
+    highlight = {
+        enable = true,
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<CR>",
+            scope_incremental = "<CR>",
+            node_incremental = "<TAB>",
+            node_decremental = "<S-TAB>",
+        },
+    },
+    indent = {
+        enable = true
+    },
+    rainbow = {
+        enable = true
+    }
+}
---
+++ nvim/lua/50-telescope.lua
@@ -0,0 +1,47 @@
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+
+require('telescope').load_extension('fzf')
+
+-- KEYBINDINGS
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fj', builtin.jumplist, {})
+
+vim.keymap.set('n', '<leader>fc', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>fs', builtin.git_stash, {})
+
+vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
