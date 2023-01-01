67472aba658b60f192c4d15d4a19d472a4c1afbd
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-01-01 20:03:22 +0100

    feat(nvim): add some keymaps from ThePrimagean

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -5,21 +5,36 @@ vim.g.mapleader = " "
 vim.keymap.set({'n', 'x', 'o'}, 'ö', '[', {remap=true, desc="pr[Ö]vious"})
 vim.keymap.set({'n', 'x', 'o'}, 'ä', ']', {remap=true, desc="n[Ä]xt"})
 
+-- paired jumps
+vim.keymap.set('n', '[q', ':cprev<CR>zz', {desc="previous [Q]uickfix entry"})
+vim.keymap.set('n', ']q', ':cnext<CR>zz', {desc="next [Q]uickfix entry"})
+vim.keymap.set('n', '[l', ':lprev<CR>zz', {desc="previous [L]ocation entry"})
+vim.keymap.set('n', ']l', ':lnext<CR>zz', {desc="next [L]ocation entry"})
+
+-- search and replace current word (case sensitive)
+vim.keymap.set('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', {desc="[S]earch and replace current word"})
+
 -- move selected lines
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", {desc="move selection down"})
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", {desc="move selection up"})
 
 --center after search
-vim.keymap.set('n', 'n', 'nzz', {desc="search [N]ext and center"})
-vim.keymap.set('n', 'N', 'Nzz', {desc="search previous and center"})
+vim.keymap.set('n', 'n', 'nzzzv', {desc="search [N]ext and center"})
+vim.keymap.set('n', 'N', 'Nzzzv', {desc="search previous and center"})
+
+-- copying and pasting
+vim.keymap.set('x', '<leader>p', '\"_dP', {desc="[P]aste without altering registers"})
+vim.keymap.set('n', '<leader>d', '\"_d', {desc="[D]elete without altering registers"})
+vim.keymap.set('v', '<leader>d', '\"_d', {desc="[D]elete without altering registers"})
 
 -- edit and apply config in dotfiles repo
 vim.keymap.set('n', '<leader>ei', '<cmd>e '..find_dotfiles()..'\\nvim<CR>', {desc="[E]dit [I]nit.lua"})
 vim.keymap.set('n', '<leader>ii', '<cmd>lua copy_config()<CR>', {desc="[I]nstall [I]nit.lua"})
 
--- TERMINAL
+-- make terminal mode and movement less weird
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 vim.keymap.set('t', '<C-w>h', '<C-\\><C-n><C-w>h')
 vim.keymap.set('t', '<C-w>j', '<C-\\><C-n><C-w>j')
 vim.keymap.set('t', '<C-w>k', '<C-\\><C-n><C-w>k')
 vim.keymap.set('t', '<C-w>l', '<C-\\><C-n><C-w>l')
+
