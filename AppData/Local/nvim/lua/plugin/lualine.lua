4c0763603e9d2f3fee90ea6e73fa9d4e4c9f5c9f
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-01-31 23:20:28 +0100

    feat(nvim): add lualine

---
+++ AppData/Local/nvim/lua/plugin/lualine.lua
@@ -0,0 +1,25 @@
+return {
+    {
+        "nvim-lualine/lualine.nvim",
+        config = function()
+            require("lualine").setup {
+                options = {
+                    theme = "zenbones"
+                },
+                sections = {
+                    lualine_b = {
+                        "branch",
+                        "diff",
+                        {
+                            "diagnostics",
+                            symbols = {error = "✘", warn = "▲", info = "", hint = "⚑"}
+                        }
+                    }
+                },
+                inactive_sections = {
+                    lualine_b = {function() return vim.fn.bufnr('%') end}
+                }
+            }
+        end
+    }
+}
