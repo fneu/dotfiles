2703aec18694c6e67907c1125fb25a54eee8dc86
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-01-31 23:19:45 +0100

    chore(nvim): remove todos from init.lua

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -3,13 +3,3 @@ require("funcs")
 require("keymaps")
 require("plugins")
 require("autocmds")
-
--- Todo:
--- lualine?
--- packer to lazy.nvim
--- neotest
--- nvim-dap
--- jump to test file function for c#
--- adjust namespaces function c#
--- rebuild solution or single projects
--- run apps either via runsettings, or hardcoded
