971254a191032f5c0949413d7b69d6cb51c717f7
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-03-17 17:55:54 +0100

    fix(nvim): darken rainbow parentheses/pairs

--- AppData/Local/nvim/lua/plugin/zenbones.lua
+++ AppData/Local/nvim/lua/plugin/zenbones.lua
@@ -23,12 +23,12 @@ return {
                         OilCreate {zenbones.diffAdded},
                         OilDelete {zenbones.diffRemoved},
                         OilMove {zenbones.diffChanged},
-                        RainbowRose {zenbones.Normal, fg=palette.rose.sa(30).li(10)},
-                        RainbowWood {zenbones.Normal, fg=palette.wood.sa(30).li(20)},
-                        RainbowWater {zenbones.Normal, fg=palette.water.sa(30).li(20)},
-                        RainbowLeaf {zenbones.Normal, fg=palette.leaf.sa(30).li(20)},
-                        RainbowBlossom {zenbones.Normal, fg=palette.blossom.sa(30).li(20)},
-                        RainbowSky {zenbones.Normal, fg=palette.sky.sa(30).li(20)},
+                        RainbowRose {zenbones.Normal, fg=palette.rose.sa(30).da(5)},
+                        RainbowWood {zenbones.Normal, fg=palette.wood.sa(30).da(5)},
+                        RainbowWater {zenbones.Normal, fg=palette.water.sa(30).da(5)},
+                        RainbowLeaf {zenbones.Normal, fg=palette.leaf.sa(30).da(5)},
+                        RainbowBlossom {zenbones.Normal, fg=palette.blossom.sa(30).da(5)},
+                        RainbowSky {zenbones.Normal, fg=palette.sky.sa(30).da(5)},
                     }
                 end
             )
