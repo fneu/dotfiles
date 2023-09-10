0d1f3108280e4c4896fc5c17ce1cc420fab568b2
Fabian Neuschmidt <fabian@neuschmidt.de>
2023-09-10 19:20:18 +0200

    fix(nvim): use powershell in such a way that :! works

--- AppData/Local/nvim/lua/options.lua
+++ AppData/Local/nvim/lua/options.lua
@@ -45,11 +45,10 @@ vim.opt.fillchars:append("fold: ")
 
 -- powershell setup according to :h powershell
 if vim.fn.has("win32") then
-    vim.o.shell = "powershell.exe"
-    vim.o.shellcmdflag =
-        "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-    vim.o.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
-    vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-    vim.o.shellquote = ""
-    vim.o.shellxquote = ""
+    vim.o.shell = vim.fn.executable('pwsh') == 1 and 'pwsh' or 'powershell'
+    vim.o.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+    vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    vim.o.shellquote = ''
+    vim.o.shellxquote = ''
 end
