cac85d06966695d445db2ceb705c3c0194fde689
Fabian Neuschmidt <fabian@neuschmidt.de>
2024-05-20 19:22:51 +0200

    feat(nvim): show hidden files in oil

--- AppData/Local/nvim/plugins/oil.lua
+++ AppData/Local/nvim/plugins/oil.lua
@@ -1,6 +1,10 @@
 MiniDeps.add("stevearc/oil.nvim")
 
-require("oil").setup()
+require("oil").setup{
+    view_options = {
+        show_hidden = true,
+    },
+}
 
 vim.keymap.set("n", "-", ":Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "_", ":e .<CR>", { desc = "Open current root directory" })
