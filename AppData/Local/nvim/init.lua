9304d58ac014d0d67aef0830b7e4bf2921e756d9
Fabian Neuschmidt <fabian@neuschmidt.de>
2024-05-24 20:33:56 +0200

    feat(nvim): local terminal settings

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -195,3 +195,13 @@ vim.api.nvim_create_autocmd(
         pattern = "*"
     }
 )
+
+-- Local settings for terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.scrolloff = 0
+  end,
+})
