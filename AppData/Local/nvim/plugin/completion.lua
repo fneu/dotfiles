021ee361a94de0379722b1c95037fe261690f0d9
Fabian Neuschmidt <fabian@neuschmidt.de>
2024-05-25 17:44:14 +0200

    feat(nvim): add luasnip and snippets

--- AppData/Local/nvim/plugin/completion.lua
+++ AppData/Local/nvim/plugin/completion.lua
@@ -6,12 +6,23 @@ MiniDeps.add({
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-nvim-lsp-signature-help",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
     },
 })
 
 MiniDeps.later(function()
     local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    luasnip.config.setup()
+    require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/snippets" })
+
     cmp.setup({
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body)
+            end,
+        },
         completion = { completeopt = "menu,menuone,noinsert" },
         mapping = cmp.mapping.preset.insert({
             ["<C-n>"] = cmp.mapping.select_next_item(),
@@ -37,15 +48,45 @@ MiniDeps.later(function()
             --  Generally you don't need this, because nvim-cmp will display
             --  completions whenever it has completion options available.
             ["<C-Space>"] = cmp.mapping.complete({}),
+
+            -- Think of <c-l> as moving to the right of your snippet expansion.
+            --  So if you have a snippet that's like:
+            --  function $name($args)
+            --    $body
+            --  end
+            --
+            -- <c-l> will move you to the right of each of the expansion locations.
+            -- <c-h> is similar, except moving you backwards.
+            ["<C-l>"] = cmp.mapping(function()
+                if luasnip.expand_or_locally_jumpable() then
+                    luasnip.expand_or_jump()
+                end
+            end, { "i", "s" }),
+            ["<C-h>"] = cmp.mapping(function()
+                if luasnip.locally_jumpable(-1) then
+                    luasnip.jump(-1)
+                end
+            end, { "i", "s" }),
         }),
         sources = {
             { name = "nvim_lsp" },
             { name = "nvim_lsp_signature_help" },
+            { name = "luasnip" },
             { name = "path" },
-            { name = "buffer" },
+            { name = "buffer", keyword_length = 4 },
         },
         formatting = {
             format = require("lspkind").cmp_format(),
         },
+    })
+
+    -- disable slow completion in xml documents
+    cmp.setup.filetype("xml", {
+        enabled = false,
+    })
+
+    -- prevent slow completion blocking cmdline
+    cmp.setup.cmdline(":", {
+        enabled = false,
     })
 end)
