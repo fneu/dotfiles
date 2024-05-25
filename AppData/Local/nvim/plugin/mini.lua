7e9de151f2ccda28d839a65bb496890f29a247bf
Fabian Neuschmidt <fabian@neuschmidt.de>
2024-05-25 22:39:35 +0200

    feat(nvim): add pairs, autotag, splitjoin with gS

--- AppData/Local/nvim/plugin/mini.lua
+++ AppData/Local/nvim/plugin/mini.lua
@@ -2,4 +2,6 @@ MiniDeps.later(function()
     require("mini.surround").setup({
         respect_selection_type = true,
     })
+    require("mini.pairs").setup()
+    require("mini.splitjoin").setup()
 end)
