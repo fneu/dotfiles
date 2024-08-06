93fcd63d994ed0d50420a95c9f5371dae8e8502c
Fabian Neuschmidt <fabian@neuschmidt.de>
2024-08-06 18:46:52 +0200

    feat(nvim): GpDiff command and binding

--- AppData/Local/nvim/plugin/gp.lua
+++ AppData/Local/nvim/plugin/gp.lua
@@ -27,4 +27,26 @@ MiniDeps.later(function()
     vim.keymap.set({ "n", "x" }, "<space>cn", ":GpChatNew popup<CR>", { remap = true, desc = "[C]opilot [N]ew" })
     vim.keymap.set({ "x" }, "<space>cp", ":GpChatPaste popup<CR>", { remap = true, desc = "[C]opilot [P]aste" })
     vim.keymap.set({ "x" }, "<space>ci", ":GpRewrite<CR>", { remap = true, desc = "[C]opilot [I]mplement" })
+    vim.keymap.set({ "x" }, "<space>cd", ":GpDiff ", { remap = true, desc = "[C]opilot rewrite to [D]iff" })
+
+    function _G.gp_diff(args, line1, line2)
+        local contents = vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), 0, -1, false)
+
+        vim.cmd("vnew")
+        local scratch_buf = vim.api.nvim_get_current_buf()
+        vim.bo[scratch_buf].buftype = "nofile"
+        vim.bo[scratch_buf].bufhidden = "wipe"
+
+        vim.api.nvim_buf_set_lines(scratch_buf, 0, -1, false, contents)
+
+        vim.cmd(line1 .. "," .. line2 .. "GpRewrite " .. args)
+
+        vim.defer_fn(function()
+            vim.cmd("diffthis")
+            vim.cmd("wincmd p")
+            vim.cmd("diffthis")
+        end, 1000)
+    end
+
+    vim.cmd("command! -range -nargs=+ GpDiff lua gp_diff(<q-args>, <line1>, <line2>)")
 end)
