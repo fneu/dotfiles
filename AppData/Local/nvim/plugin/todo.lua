1f47c9772378e619ea880c35edeae6853c116fe3
Fabian Neuschmidt <fabian@neuschmidt.de>
2025-01-22 10:34:08 +0100

    feat(nvim): always highlight TODO, WARN, NOTE in markdown and text files

--- AppData/Local/nvim/plugin/todo.lua
+++ AppData/Local/nvim/plugin/todo.lua
@@ -15,4 +15,14 @@ MiniDeps.later(function()
     end, { desc = "pr[Ö]vious [T]odo comment" })
 
     vim.keymap.set("n", "<leader>ft", ":TodoTelescope<CR>", { desc = "[F]ind [T]odo-commends" })
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+        desc = "Enable todo-comments for text",
+        group = vim.api.nvim_create_augroup("user.todo.text", { clear = true }),
+        callback = function(ev)
+            local config = require("todo-comments.config")
+            local comments_only = string.match(ev.file, "%.md$") == nil and string.match(ev.file, "%.txt$") == nil
+            config.options.highlight.comments_only = comments_only
+        end,
+    })
 end)
